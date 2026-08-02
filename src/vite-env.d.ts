c6ab91a164be8140bf82dcb306fd6c014bb6ea4a
6dhsf7y9zj-creator <309794804+6dhsf7y9zj-creator@users.noreply.github.com>
2026-08-02 01:41:53 +0100

    Add explicit CSS module declaration
    Add explicit CSS module declaration

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,7 +1,2 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: '/jos-one-react/',
-})
+/// <reference types="vite/client" />
+declare module '*.css';
