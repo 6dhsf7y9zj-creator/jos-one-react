f947da9663c31d04c8f329cdcc6b91a3ae91f3d7
6dhsf7y9zj-creator <309794804+6dhsf7y9zj-creator@users.noreply.github.com>
2026-08-02 01:57:48 +0100

    Fix React startup
    Fix React startup

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode><App /></React.StrictMode>
-);
+const root = document.getElementById('root')
+
+if (!root) {
+  throw new Error('Root element was not found')
+}
+
+createRoot(root).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
